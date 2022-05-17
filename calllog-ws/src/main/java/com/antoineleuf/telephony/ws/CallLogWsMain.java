bcefcfedc74531bf83b955653cb16643a5af8276
Antoine Lefrancois <antoine.lefrancois@outlook.com>
2022-05-16 22:57:48 -0400

    Change contact path for gateway in calllog service

--- calllog-ws/src/main/java/com/antoineleuf/telephony/ws/CallLogWsMain.java
+++ calllog-ws/src/main/java/com/antoineleuf/telephony/ws/CallLogWsMain.java
@@ -26,79 +26,77 @@ import com.antoineleuf.telephony.ws.infrastructure.CallLogDevDataFactory;
 import com.antoineleuf.telephony.ws.infrastructure.CallLogRepositoryInMemory;
 import com.antoineleuf.telephony.ws.infrastructure.contact.ContactRestClient;
 
-
 /**
  * RESTApi for the call log service
  *
  */
 @SuppressWarnings("all")
-public class CallLogWsMain 
-{
-   
-   private static final int WS_PORT = 8081; // HTTP port on which the web service is listening
+public class CallLogWsMain {
 
-  private static final String CONTACT_WS_URL = "http://127.0.0.1:8080/api/"; // URL of the gateway web app
-    
-   public static boolean isDev = true; // Would be a JVM argument or in a .property file
+  private static final int WS_PORT = 8081; // HTTP port on which the web service is listening
 
-    public static void main(String[] args)
-            throws Exception {
+  private static final String CONTACT_WS_URL = "http://127.0.0.1:8080/api/telephony"; // URL of the
+                                                                                      // gateway web
+                                                                                      // app
 
-      // Setup resources (API)
-      CallLogResource callLogResource = createCallLogResource();
+  public static boolean isDev = true; // Would be a JVM argument or in a .property file
 
-      // Setup API context (JERSEY + JETTY)
-      ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-      context.setContextPath("/api/");
-      ResourceConfig resourceConfig = ResourceConfig.forApplication(new Application() {
-        @Override
-        public Set<Object> getSingletons() {
-          HashSet<Object> resources = new HashSet<>();
-          // Add resources to context
-          resources.add(callLogResource);
-          return resources;
-        }
-      });
+  public static void main(String[] args) throws Exception {
 
+    // Setup resources (API)
+    CallLogResource callLogResource = createCallLogResource();
 
-      ServletContainer servletContainer = new ServletContainer(resourceConfig);
-      ServletHolder servletHolder = new ServletHolder(servletContainer);
-      context.addServlet(servletHolder, "/*");
-
-      // Setup static file context (WEBAPP)
-      WebAppContext webapp = new WebAppContext();
-      webapp.setResourceBase("src/main/webapp");
-      webapp.setContextPath("/");
-
-      // Setup http server
-      ContextHandlerCollection contexts = new ContextHandlerCollection();
-      contexts.setHandlers(new Handler[] { context, webapp });
-      Server server = new Server(WS_PORT);
-      server.setHandler(contexts);
-
-      try {
-        server.start();
-        server.join();
-      } finally {
-        server.destroy();
+    // Setup API context (JERSEY + JETTY)
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setContextPath("/api/");
+    ResourceConfig resourceConfig = ResourceConfig.forApplication(new Application() {
+      @Override
+      public Set<Object> getSingletons() {
+        HashSet<Object> resources = new HashSet<>();
+        // Add resources to context
+        resources.add(callLogResource);
+        return resources;
       }
+    });
+
+    ServletContainer servletContainer = new ServletContainer(resourceConfig);
+    ServletHolder servletHolder = new ServletHolder(servletContainer);
+    context.addServlet(servletHolder, "/*");
+
+    // Setup static file context (WEBAPP)
+    WebAppContext webapp = new WebAppContext();
+    webapp.setResourceBase("src/main/webapp");
+    webapp.setContextPath("/");
+
+    // Setup http server
+    ContextHandlerCollection contexts = new ContextHandlerCollection();
+    contexts.setHandlers(new Handler[] { context, webapp });
+    Server server = new Server(WS_PORT);
+    server.setHandler(contexts);
+
+    try {
+      server.start();
+      server.join();
+    } finally {
+      server.destroy();
+    }
+  }
+
+  private static CallLogResource createCallLogResource() {
+    // Setup resources' dependencies (DOMAIN + INFRASTRUCTURE)
+    CallLogRepository callLogRepository = new CallLogRepositoryInMemory();
+
+    // For development ease
+    if (isDev) {
+      CallLogDevDataFactory callLogDevDataFactory = new CallLogDevDataFactory();
+      List<CallLog> callLogs = callLogDevDataFactory.createMockData();
+      callLogs.stream().forEach(callLogRepository::save);
     }
 
-    private static CallLogResource createCallLogResource() {
-      // Setup resources' dependencies (DOMAIN + INFRASTRUCTURE)
-      CallLogRepository callLogRepository = new CallLogRepositoryInMemory();
+    CallLogAssembler callLogAssembler = new CallLogAssembler();
+    ContactRepository contactRepository = new ContactRestClient(CONTACT_WS_URL);
+    CallLogService callLogService = new CallLogService(callLogRepository, callLogAssembler, contactRepository);
 
-      // For development ease
-      if (isDev) {
-        CallLogDevDataFactory callLogDevDataFactory = new CallLogDevDataFactory();
-        List<CallLog> callLogs = callLogDevDataFactory.createMockData();
-        callLogs.stream().forEach(callLogRepository::save);
-      }
-
-      CallLogAssembler callLogAssembler = new CallLogAssembler();
-      ContactRepository contactRepository = new ContactRestClient(CONTACT_WS_URL);
-      CallLogService callLogService = new CallLogService(callLogRepository, callLogAssembler, contactRepository );
-
-      return new CallLogResourceImpl(callLogService);
-    }
+    return new CallLogResourceImpl(callLogService);
+  }
 }
